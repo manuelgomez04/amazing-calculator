895c2e554885ce32d16bdbfafa3a65b2e13294f9
Manuel Gómez <gomez.maman24@triana.salesianos.edu>
2023-11-16 10:45:41 +0100

    Realizamos la suma

--- IncredibleCalculator.java
+++ IncredibleCalculator.java
@@ -1,6 +1,9 @@
 import java.util.Scanner;
 
 public class IncredibleCalculator {
+    String aux;
+    int num1, num2, sum, resta, product, division, productDecim;
+    double num3, num4;
 
     public static void main(String[] args) {
 
@@ -15,15 +18,68 @@ public class IncredibleCalculator {
 
 
         switch (opcion) {
-            case 1: //Sumar. Debes leer dos numeros enteros de entrada e imprimir su suma
+            case 1: 
+                //Sumar. Debes leer dos numeros enteros de entrada e imprimir su suma
+                System.out.println ("Diga número 1");
+                aux=sc.nextLine();
+                num1= Integer.parseInt(aux);
+                System.out.println ("Diga número 2");
+                aux=sc.nextLine();
+                num2= Integer.parseInt(aux);
+
+                sum = num1+num2;
+
+                System.out.println ("El resultado de la suma es"+ suma);
+
                     break;
-            case 2: // Restar. Debes leer dos números enteros de entrada e imprimir su resta
+ /*           case 2: // Restar. Debes leer dos números enteros de entrada e imprimir su resta
+                System.out.println ("Diga número 1");
+                aux=sc.nextLine();
+                num1= Integer.parseInt(aux);
+                System.out.println ("Diga número 2");
+                aux=sc.nextLine();
+                num2= Integer.parseInt(aux);
+
+                resta = num1-num2;
+
+                System.out.println ("El resultado de la suma es"+ resta);
                     break;
             case 3: // Multiplicar. Debes leer dos números enteros de entrada e imprimir su producto.
+
+                System.out.println ("Diga número 1");
+                aux=sc.nextLine();
+                num1= Integer.parseInt(aux);
+                System.out.println ("Diga número 2");
+                aux=sc.nextLine();
+                num2= Integer.parseInt(aux);
+
+                produc = num1*num2;
+
+                System.out.println ("El resultado del producto es "+ produc);
                     break;
             case 4: // Multiplicar. Debes leer dos números enteros de entrada e imprimir su división entera.
+                System.out.println ("Diga número 1");
+                aux=sc.nextLine();
+                num1= Integer.parseInt(aux);
+                System.out.println ("Diga número 2");
+                aux=sc.nextLine();
+                num2= Integer.parseInt(aux);
+
+                division = num1/num2;
+
+                 System.out.println ("El resultado de la division es"+ division);
                     break;
             case 5: // Multiplicar. Debes leer dos números enteros de entrada e imprimir su división con decimales.
+                System.out.println ("Diga número 1");
+                aux=sc.nextLine();
+                num3= Double.parseDouble(aux);
+                System.out.println ("Diga número 2");
+                aux=sc.nextLine();
+                num4= Double.parseDouble(aux);
+
+                productDecim = num3 * num4;
+
+                 System.out.printf ("El resultado de la multiplicación de enteros es %.2f", productDecim) ;
                     break;
 
         }
@@ -60,8 +116,8 @@ public class IncredibleCalculator {
             System.err.println("Lo que has introducido no es un número entero!");
             System.exit(1);
             return -1;
-        }
+        }*/
 
-    }
+    } 
 
 }
